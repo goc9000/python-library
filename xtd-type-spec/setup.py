935a16b6d30599d272aad1416a711ca68dbe3b0e
Atom of Justice <atmfjstc@protonmail.com>
2020-03-17 02:19:38 +0200

    Add initial commit for 'xtd-type-spec' package

---
+++ xtd-type-spec/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='atmfjstc-xtd-type-spec',
+    version='1.0.0',
+
+    author_email='atmfjstc@protonmail.com',
+
+    package_dir={'': 'src'},
+    packages=['atmfjstc.lib.xtd_type_spec'],
+
+    install_requires=[
+        'atmfjstc-py-lang-utils>=1, <2',
+    ],
+
+    zip_safe=True,
+
+    description="An extended type specification and improved isinstance/issubclass variants to go with it",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ xtd-type-spec/src/atmfjstc/lib/xtd_type_spec/__init__.py
@@ -0,0 +1,112 @@
+"""
+Provides an extended type specification mechanism that offers some extra features vs. just using a <type> object.
+
+Normally, in Python, when we want to programmatically specify a type against which a value can be typechecked at
+runtime, we can just use a <type> instance and then use ``isinstance(value, type)``. E.g::
+
+    ALLOWED_TYPE = str
+
+    value = 3
+    if not isinstance(value, ALLOWED_TYPE):
+        raise TypeError("Value type is incorrect")
+
+There are, however, some type specifications that a <type> object cannot represent:
+
+- Union types, i.e. where a value is allowed to be either of a number of types. E.g. "int | string | float"
+- Constant types, where a variable must have a specific value to fit the "type". This is mostly useful when combined
+  with the union type, as we can thus limit a value to any of a number of choices, e.g. "1 | 2 | 3". This can also
+  represent optional types, e.g. "str | None"
+
+Python's built-in ``isinstance`` and ``issubclass`` offer some support for union types, but not for constant types and
+combinations thereof. Additionally, ``issubclass`` only allows a regular (non-union) type on the left side.
+
+This module provides some utilities for working with an "extended" way of specifying types. Specifically, it offers
+the ``isinstance_ex`` and ``issubclass_ex`` functions, that mimic the interface of the builtins but support a richer
+syntax for expressing types:
+
+- a normal type like ``str``, ``list`` etc. will stand for itself as before
+- lists or tuples will be treated as union types, e.g. ``(list, str)`` means list|str
+- a hashable, non-class value will be treated as a constant type, e.g. ``(bool, "other")`` will produce a type that
+  accepts ``True``, ``False`` or the string ``'other'``
+- the tokens ``AnyType`` and ``VoidType`` stand for the universal type (any value matches) and null type (no
+  value matches) respectively.
+- these idioms can be combined recursively, e.g. ``(list, (1, 2, bool))`` although it should be noted that all such
+  expressions ultimately reduce to a flat tuple under this simple type system
+- one can use an idiom like ``(type, None)`` to represent an optional type
+"""
+
+from collections.abc import Sequence, Hashable
+
+from atmfjstc.lib.py_lang_utils.token import Token
+
+
+AnyType = Token(repr_='AnyType')
+"""This is used as a token for the universal type (any value matches)."""
+
+
+VoidType = Token(repr_='VoidType')
+"""This is used as a token for the null/void type (no value matches)."""
+
+
+def isinstance_ex(value, xtd_type_spec):
+    """
+    Checks whether the given value conforms to the type described by ``xtd_type_spec``.
+
+    See the module description for details on the format of the extended type specification.
+
+    Designed as a drop-in replacement for ``isinstance()``
+    """
+    if isinstance(xtd_type_spec, type):
+        return isinstance(value, xtd_type_spec)
+    elif xtd_type_spec == AnyType:
+        return True
+    elif xtd_type_spec == VoidType:
+        return False
+    elif _is_sequence(xtd_type_spec):
+        return any(isinstance_ex(value, alt) for alt in xtd_type_spec)
+    elif isinstance(xtd_type_spec, Hashable):
+        return value == xtd_type_spec
+    else:
+        raise TypeError("Invalid extended type specification: {}".format(xtd_type_spec))
+
+
+def issubclass_ex(xtd_type_spec, parent_type_spec):
+    """
+    Checks whether the given type is a subtype of another, i.e. all values that would fit ``xtd_type_spec`` will also
+    fit ``parent_type_spec``
+
+    See the module description for details on the format of the extended type specification.
+
+    Designed as a drop-in replacement for ``issubclass()``
+    """
+    if (parent_type_spec == AnyType) or (xtd_type_spec == VoidType):
+        return True  # Note that this also covers Any vs Any and Void vs Void (both true)
+    if (parent_type_spec == VoidType) or (xtd_type_spec == AnyType):
+        return False
+
+    if _is_sequence(xtd_type_spec):
+        return all(issubclass_ex(alt, parent_type_spec) for alt in xtd_type_spec)
+    if _is_sequence(parent_type_spec):
+        return any(issubclass_ex(xtd_type_spec, alt) for alt in parent_type_spec)
+
+    if isinstance(parent_type_spec, type):
+        if isinstance(xtd_type_spec, type):
+            return issubclass(xtd_type_spec, parent_type_spec)
+        elif isinstance(xtd_type_spec, Hashable):
+            return isinstance(xtd_type_spec, parent_type_spec)
+        else:
+            raise TypeError("Invalid extended type specification: {}".format(xtd_type_spec))
+
+    if isinstance(parent_type_spec, Hashable):
+        if isinstance(xtd_type_spec, type):
+            return False
+        elif isinstance(xtd_type_spec, Hashable):
+            return xtd_type_spec == parent_type_spec
+        else:
+            raise TypeError("Invalid extended type specification: {}".format(xtd_type_spec))
+
+    raise TypeError("Invalid extended type specification: {}".format(parent_type_spec))
+
+
+def _is_sequence(value):
+    return isinstance(value, Sequence) and not isinstance(value, str)
