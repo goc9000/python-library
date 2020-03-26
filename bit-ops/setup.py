b2bdb5dbc314d1d7f848175678204d3311ea37bd
Atom of Justice <atmfjstc@protonmail.com>
2020-03-26 22:40:39 +0200

    Create bit manipulation library

---
+++ bit-ops/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='atmfjstc-bit-ops',
+    version='0.1.0',
+
+    author_email='atmfjstc@protonmail.com',
+
+    package_dir={'': 'src'},
+    packages=['atmfjstc.lib.bit_ops'],
+
+    zip_safe=True,
+
+    description="Advanced bit manipulation utilities",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ bit-ops/src/atmfjstc/lib/bit_ops/__init__.py
@@ -0,0 +1,25 @@
+"""
+A collection of utilities for bit manipulation, similar to those in Bit Twiddling Hacks or Hacker's Delight.
+
+Note that owing to the interpreted nature of Python, these cannot possibly achieve any degree of efficiency. For
+processing large amounts of data, it's better to use ``numpy`` or some other library.
+
+Reference: https://graphics.stanford.edu/~seander/bithacks.html
+"""
+
+import math
+
+
+def get_set_bits(value):
+    """
+    Returns a list of all set bits in an integer. E.g. for 24, returns [3,4] since 24 = (1 << 3) + (1 << 4)
+    """
+    value = abs(value)
+
+    result = []
+    while value > 0:
+        next_value = value & (value - 1)
+        result.append(round(math.log2(value - next_value)))
+        value = next_value
+
+    return result
