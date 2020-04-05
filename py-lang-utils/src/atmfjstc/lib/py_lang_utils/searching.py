28174f434e10ac5f4d1c7ae959024597314e4e48
Atom of Justice <atmfjstc@protonmail.com>
2020-04-05 21:06:22 +0300

    Add reverse searching utility

--- py-lang-utils/src/atmfjstc/lib/py_lang_utils/searching.py
+++ py-lang-utils/src/atmfjstc/lib/py_lang_utils/searching.py
@@ -1,3 +1,6 @@
+from collections.abc import Sequence
+
+
 def index_where(seq, callback):
     """
     Returns the first index in a sequence (list, tuple, stream etc) for which a callback applied to its element holds
@@ -13,3 +16,27 @@ def index_where(seq, callback):
             return index
 
     return None
+
+
+def last_index_where(seq, callback):
+    """
+    Returns the last index in a sequence (list, tuple, stream etc) for which a callback applied to its element holds
+    true. If the callback never holds true for any item in the sequence, returns None.
+
+    Note that the function also works on streams. However, an infinite stream will result in an infinite loop!
+    """
+    # Optimized version for vector-likes
+    if isinstance(seq, Sequence):
+        for index in range(len(seq) - 1, -1, -1):
+            if callback(seq[index]):
+                return index
+
+        return None
+
+    last_index = None
+
+    for index, item in enumerate(seq):
+        if callback(item):
+            last_index = index
+
+    return last_index
