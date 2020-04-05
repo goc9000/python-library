f690b8d723ddf7d942831321dffdd72f450d24b4
Atom of Justice <atmfjstc@protonmail.com>
2020-04-05 19:28:26 +0300

    Add PromptableNode abstraction

--- abstract-codegen/src/atmfjstc/lib/abstract_codegen/ast/base.py
+++ abstract-codegen/src/atmfjstc/lib/abstract_codegen/ast/base.py
@@ -14,4 +14,26 @@ class AbstractCodegenASTNode(ASTNode, metaclass=ABCMeta):
         """
         A generator that yields every line in the rendered representation of this node.
         """
+
+
+class PromptableNode(AbstractCodegenASTNode):
+    """
+    Base class for nodes that are capable of rendering within a specific kind of non-rectangular space.
+
+    Normally nodes attempt to render within a rectangular space of context.width columns across. In some situations,
+    however, the real available space has a special shape: it is a rectangle of context.width columns across as before,
+    but some of the leftmost columns in first line are reserved (this space is called the prompt), as well as some of
+    the rightmost columns in the last line (this space is called the tail). This often happens for items inside an
+    array (e.g. they need to reserve space for the comma) or object properties (the property name is the prompt).
+
+    Since only certain kinds of structures can support this sort of complex rendering, we mark them by making them
+    derive from this node type.
+    """
+    AST_NODE_CONFIG = ('abstract',)
+
+    def render(self, context):
+        return self.render_promptable(context, 0, 0)
+
+    @abstractmethod
+    def render_promptable(self, context, prompt_width, tail_width):
         pass
