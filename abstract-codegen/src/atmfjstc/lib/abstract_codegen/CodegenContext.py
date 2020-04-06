6f23df5ab7a210f6b4b8cba5a9dce5b316bdebe2
Atom of Justice <atmfjstc@protonmail.com>
2020-04-06 11:45:06 +0300

    Add 'oneliner' field to context

--- abstract-codegen/src/atmfjstc/lib/abstract_codegen/CodegenContext.py
+++ abstract-codegen/src/atmfjstc/lib/abstract_codegen/CodegenContext.py
@@ -1,10 +1,12 @@
 class CodegenContext:
     _width = None
     _indent = None
+    _oneliner = None  # Request a oneliner rendering if available
 
-    def __init__(self, width, indent=2):
+    def __init__(self, width, indent=2, oneliner=False):
         self._width = width
         self._indent = indent
+        self._oneliner = oneliner
 
     @property
     def width(self):
@@ -14,11 +16,16 @@ class CodegenContext:
     def indent(self):
         return self._indent
 
-    def derive(self, width=None, indent=None, add_width=0, sub_width=0, sub_one_indent=False):
+    @property
+    def oneliner(self):
+        return self._oneliner
+
+    def derive(self, width=None, indent=None, add_width=0, sub_width=0, sub_one_indent=False, oneliner=None):
         def coalesce(a, b):
             return a if b is None else b
 
         return CodegenContext(
             width=coalesce(self.width, width) + add_width - sub_width - (self.indent if sub_one_indent else 0),
             indent=coalesce(self.indent, indent),
+            oneliner=coalesce(self.oneliner, oneliner),
         )
