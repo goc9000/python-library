7dada1ea922f85be6dace863606089b0289880a7
Atom of Justice <atmfjstc@protonmail.com>
2020-04-14 17:55:41 +0300

    Added dict_no_nulls utility

--- py-lang-utils/src/atmfjstc/lib/py_lang_utils/dict.py
+++ py-lang-utils/src/atmfjstc/lib/py_lang_utils/dict.py
@@ -13,3 +13,21 @@ def copy_only_fields(source_dict, fields):
     For more advanced processing, consider the `convert_struct` module in the same package.
     """
     return source_dict.__class__((k, v) for k, v in source_dict.items() if k in fields)
+
+
+def dict_no_nulls(source_dict, *nulls):
+    """
+    Creates a copy of a dict with only non-null fields preserved. By default, a field is null if it is None.
+
+    To specify other values that be considered null (e.g. False, 0, etc.), add them as arguments after the dict. Note
+    that you will have to re-specify None if you still want it to be a null indicator.
+
+    Other types of mappings will work too, but they must have a dict-like constructor that accepts an iterable of
+    key-value pairs.
+
+    For more advanced processing, consider the `convert_struct` module in the same package.
+    """
+    if len(nulls) == 0:
+        nulls = {None}
+
+    return source_dict.__class__((k, v) for k, v in source_dict.items() if v not in nulls)
