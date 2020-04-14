e00640ef774d03ee525f8d91e36af7dd239d4fc6
Atom of Justice <atmfjstc@protonmail.com>
2020-04-14 17:55:31 +0300

    Improve copy_only_fields to work with any mapping

--- py-lang-utils/src/atmfjstc/lib/py_lang_utils/dict.py
+++ py-lang-utils/src/atmfjstc/lib/py_lang_utils/dict.py
@@ -1,7 +1,15 @@
 """
-Miscellaneous utilities for working with dicts.
+Miscellaneous utilities for working with dicts and mappings.
 """
 
 
 def copy_only_fields(source_dict, fields):
-    return { k: v for k, v in source_dict.items() if k in fields }
+    """
+    Creates a copy of a dict with only certain fields preserved.
+
+    Other types of mappings will work too, but they must have a dict-like constructor that accepts an iterable of
+    key-value pairs.
+
+    For more advanced processing, consider the `convert_struct` module in the same package.
+    """
+    return source_dict.__class__((k, v) for k, v in source_dict.items() if k in fields)
