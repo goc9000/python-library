a0b90873cde456417d68af1492ea4824d52659eb
Atom of Justice <atmfjstc@protonmail.com>
2020-04-16 13:34:18 +0300

    Forgot to allow predicate for a nulls= value

---
+++ py-lang-utils/src/atmfjstc/lib/py_lang_utils/functions.py
@@ -0,0 +1,14 @@
+"""
+Utilities for working with functions.
+"""
+
+
+def seems_callback(value):
+    """
+    Checks whether a value looks like a callback.
+
+    This function cannot possibly be very accurate without resorting to expensive inspect calls. What it does is check
+    whether the value is a 'function' or 'method' (so as to distinguish it from other callables like classes). It is
+    meant for distinguishing callback arguments (e.g. predicates) from non-functional ones.
+    """
+    return value.__class__.__name__ in ['function', 'method']
