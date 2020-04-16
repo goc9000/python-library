a8464a679bda2ac1e5e6cfa4e28b9848b88f18ff
Atom of Justice <atmfjstc@protonmail.com>
2020-04-16 13:17:26 +0300

    Upgrade filter_dict_nulls function

--- py-lang-utils/src/atmfjstc/lib/py_lang_utils/dict.py
+++ py-lang-utils/src/atmfjstc/lib/py_lang_utils/dict.py
@@ -2,6 +2,10 @@
 Miscellaneous utilities for working with dicts and mappings.
 """
 
+from collections.abc import Mapping
+
+from atmfjstc.lib.py_lang_utils.empty import make_null_test
+
 
 def copy_only_fields(source_dict, fields):
     """
@@ -15,19 +19,21 @@ def copy_only_fields(source_dict, fields):
     return source_dict.__class__((k, v) for k, v in source_dict.items() if k in fields)
 
 
-def filter_dict_nulls(source_dict, *nulls):
+def filter_dict_nulls(source_dict, nulls=None):
     """
     Creates a copy of a dict with only non-null fields preserved. By default, a field is null if it is None.
 
-    To specify other values that be considered null (e.g. False, 0, etc.), add them as arguments after the dict. Note
-    that you will have to re-specify None if you still want it to be a null indicator.
+    To specify other values that should be considered null (e.g. False, 0, etc.), put them in a sequence and pass it
+    via the ``nulls=`` parameter. Note that you will have to re-specify None if you still want it to be a null
+    indicator. As an extra, you can specify an empty dict or an empty list as null values to filter out any empty
+    Mapppings or Sequences (even if they are not hashable). Finally, you can also pass a function in the ``nulls=``
+    parameter that should return True for null values.
 
     Other types of mappings will work too, but they must have a dict-like constructor that accepts an iterable of
     key-value pairs.
 
     For more advanced processing, consider the `convert_struct` module in the same package.
     """
-    if len(nulls) == 0:
-        nulls = {None}
+    null_test = make_null_test(nulls)
 
-    return source_dict.__class__((k, v) for k, v in source_dict.items() if v not in nulls)
+    return source_dict.__class__((k, v) for k, v in source_dict.items() if not null_test(v))
---
+++ py-lang-utils/src/atmfjstc/lib/py_lang_utils/empty.py
@@ -0,0 +1,53 @@
+"""
+Utilities for working with nulls, empty values etc.
+"""
+
+from collections.abc import Mapping, Sequence, Hashable
+
+
+def make_null_test(nulls):
+    """
+    Helper that creates a function that tests whether its value is null, according to the following rules:
+
+    - If ``nulls`` is None or empty, only None values are considered null
+    - Otherwise, ``nulls`` must be a sequence of values that are considered null. The items must be Hashable, and only
+      Hashable values will be tested, with two exceptions:
+    - You can specify an empty list() or an empty dict() as null values. In this case, non-Hashable Sequences and
+      Mappings, respectively, will also be considered null.
+    """
+    if (nulls is None) or len(nulls) == 0:
+        return _is_none
+
+    nulls_set = set()
+    empty_seqs_are_null = False
+    empty_maps_are_null = False
+
+    for null in nulls:
+        if isinstance(null, Hashable):
+            nulls_set.add(null)
+        elif isinstance(null, list):
+            if len(null) > 0:
+                raise TypeError("lists passed as nulls must be empty")
+            empty_seqs_are_null = True
+        elif isinstance(null, dict):
+            if len(null) > 0:
+                raise TypeError("dicts passed as nulls must be empty")
+            empty_maps_are_null = True
+        else:
+            raise TypeError(f"Invalid null value (not hashable): {null!r}")
+
+    def _test(value):
+        if isinstance(value, Hashable):
+            return value in nulls_set
+        if isinstance(value, Sequence) and empty_seqs_are_null and len(value) == 0:
+            return True
+        if isinstance(value, Mapping) and empty_maps_are_null and len(value) == 0:
+            return True
+
+        return False
+
+    return _test
+
+
+def _is_none(value):
+    return value is None
