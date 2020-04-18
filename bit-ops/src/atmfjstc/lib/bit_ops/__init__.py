4fbcb4a36c40cdbdaee16ad3cf7092a674a1f570
Atom of Justice <atmfjstc@protonmail.com>
2020-04-19 02:10:07 +0300

    Add typing for bit-ops package

--- bit-ops/src/atmfjstc/lib/bit_ops/__init__.py
+++ bit-ops/src/atmfjstc/lib/bit_ops/__init__.py
@@ -2,15 +2,17 @@
 A collection of utilities for bit manipulation, similar to those in Bit Twiddling Hacks or Hacker's Delight.
 
 Note that owing to the interpreted nature of Python, these cannot possibly achieve any degree of efficiency. For
-processing large amounts of data, it's better to use ``numpy`` or some other library.
+processing large amounts of data, it's better to use `numpy` or some other library.
 
 Reference: https://graphics.stanford.edu/~seander/bithacks.html
 """
 
 import math
 
+from typing import List
 
-def get_set_bits(value):
+
+def get_set_bits(value: int) -> List[int]:
     """
     Returns a list of all set bits in an integer. E.g. for 24, returns [3,4] since 24 = (1 << 3) + (1 << 4)
     """
