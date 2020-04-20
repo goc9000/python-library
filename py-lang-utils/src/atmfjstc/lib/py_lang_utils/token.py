d73829b1ad24bd5c25d00fa5fa74e64482a737d4
Atom of Justice <atmfjstc@protonmail.com>
2020-04-20 15:30:31 +0300

    Add typing and docs for Token class

--- py-lang-utils/src/atmfjstc/lib/py_lang_utils/token.py
+++ py-lang-utils/src/atmfjstc/lib/py_lang_utils/token.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Token:
     """
     A class for producing tokens, i.e. values that compare equal to nothing else but themselves.
@@ -7,12 +10,19 @@ class Token:
     _str = None
     _repr = None
 
-    def __init__(self, str_=None, repr_=None):
+    def __init__(self, str_: Optional[str] = None, repr_: Optional[str] = None):
+        """
+        Produces a new token.
+
+        Args:
+            str_: If not None, the text the token should produce when `str()` is called on it
+            repr_: If not None, the text the token should produce when `repr()` is called on it
+        """
         self._str = str_
         self._repr = repr_
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self._str if (self._str is not None) else super().__str__()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self._repr if (self._repr is not None) else super().__repr__()
