8fa6c0b3af3ad9facb0bc05aa21982d31589c68b
Atom of Justice <atmfjstc@protonmail.com>
2020-04-23 20:34:55 +0300

    Created stub for binary-utils package

---
+++ binary-utils/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='atmfjstc-binary-utils',
+    version='0.0.0',
+
+    author_email='atmfjstc@protonmail.com',
+
+    package_dir={'': 'src'},
+    packages=['atmfjstc.lib.binary_utils'],
+
+    zip_safe=True,
+
+    description="Utilities for parsing binary and bit-level data",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Typing :: Typed",
+    ],
+    python_requires='>=3.7',
+)
---
+++ binary-utils/src/atmfjstc/lib/binary_utils/__init__.py
@@ -0,0 +1,10 @@
+"""
+Utilities for parsing binary and bit-level data.
+
+This package can come in useful if you need to parse binary format files (e.g. ZIP archives) or operating system data
+in binary format.
+
+Note that owing to the interpreted nature of Python, these utilities are intrinsically very inefficient. They are meant
+to handle files with hundreds or thousands of records at most. For larger data sets, it is best to consider using
+modules written in C or some other systems programming language.
+"""
