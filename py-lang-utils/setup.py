767944fbecad57c3abcf2120ff9701399023d10a
Atom of Justice <atmfjstc@protonmail.com>
2020-04-30 18:10:04 +0300

    Add fundamental package notice

--- py-lang-utils/setup.py
+++ py-lang-utils/setup.py
@@ -9,6 +9,8 @@ setup(
     package_dir={'': 'src'},
     packages=['atmfjstc.lib.py_lang_utils'],
 
+    # Note to self: think twice before adding any install_requires here! This is intended to be a fundamental package.
+
     zip_safe=True,
 
     description="Atom of Justice's collection of Python language idioms and helpers for any application",
