422f68c46e5385832b2d517635309f7fba587fe0
Atom of Justice <atmfjstc@protonmail.com>
2020-05-14 01:33:37 +0300

    Add data types for storing Windows NT Security Descriptors

--- os-forensics/src/atmfjstc/lib/os_forensics/windows/security/__init__.py
+++ os-forensics/src/atmfjstc/lib/os_forensics/windows/security/__init__.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Optional, Tuple
+
+from atmfjstc.lib.os_forensics.windows.security.rights import NTSpecificRights
 
 
 @dataclass(frozen=True)
@@ -29,3 +31,112 @@ class NTGuid:
 
     def __str__(self):
         return f"{self.group1:08X}-{self.group2:04X}-{self.group3:04X}-{self.group4:04X}-{self.group5:012X}"
+
+
+@dataclass(frozen=True)
+class NTSecurityDescriptor:
+    owner: 'NTSdTrusteeInfo'
+    group: 'NTSdTrusteeInfo'
+
+    dacl: 'NTSdACLInfo'
+    sacl: 'NTSdACLInfo'
+
+    rm_control_valid: bool
+
+
+@dataclass(frozen=True)
+class NTSdTrusteeInfo:
+    sid: Optional[NTSecurityID]
+    defaulted: bool
+
+
+@dataclass(frozen=True)
+class NTSdACLInfo:
+    present: bool
+    null: bool
+    entries: Tuple['NTAclEntry', ...]
+    defaulted: bool
+    auto_inherit_req: bool
+    auto_inherited: bool
+    protected: bool
+
+
+@dataclass(frozen=True)
+class NTAclEntry:
+    trustee: NTSecurityID
+    inherit: 'NTAclEntryInheritFlags'
+
+
+@dataclass(frozen=True)
+class NTAclEntryInheritFlags:
+    objects: bool
+    containers: bool
+    no_propagate: bool
+    inherit_only: bool
+    inherited: bool
+
+
+@dataclass(frozen=True)
+class NTDiscretionaryACLEntry(NTAclEntry):
+    pass
+
+
+@dataclass(frozen=True)
+class NTRegularDACLEntryBase(NTDiscretionaryACLEntry):
+    standard_rights: 'NTStandardRights'
+    specific_rights: NTSpecificRights
+    object_type: Optional[NTGuid]
+    inherited_object_type: Optional[NTGuid]
+    callback_data: Optional[bytes]
+
+
+@dataclass(frozen=True)
+class NTAllowAccessACLEntry(NTRegularDACLEntryBase):
+    pass
+
+
+@dataclass(frozen=True)
+class NTDenyAccessACLEntry(NTRegularDACLEntryBase):
+    pass
+
+
+@dataclass(frozen=True)
+class NTSystemACLEntry(NTAclEntry):
+    pass
+
+
+@dataclass(frozen=True)
+class NTRegularSACLEntryBase(NTSystemACLEntry):
+    standard_audited_ops: 'NTStandardRights'
+    specific_audited_ops: 'NTSpecificRights'
+    object_type: Optional[NTGuid]
+    inherited_object_type: Optional[NTGuid]
+    callback_data: Optional[bytes]
+
+
+@dataclass(frozen=True)
+class NTSystemAuditACLEntry(NTRegularSACLEntryBase):
+    audit_success: bool
+    audit_failure: bool
+    audit_sacl: bool
+
+
+@dataclass(frozen=True)
+class NTSystemAlarmACLEntry(NTRegularSACLEntryBase):
+    pass
+
+
+@dataclass(frozen=True)
+class NTMandatoryLabelACLEntry(NTSystemACLEntry):
+    no_write_up: bool
+    no_read_up: bool
+    no_execute_up: bool
+
+
+@dataclass(frozen=True)
+class NTStandardRights:
+    delete: bool
+    read_control: bool
+    write_dacl: bool
+    write_owner: bool
+    synchronize: bool
