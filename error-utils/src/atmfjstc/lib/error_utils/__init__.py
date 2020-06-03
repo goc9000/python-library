2f796dd922ee950a29a26fc4b7718532e65b038b
Atom of Justice <atmfjstc@protonmail.com>
2020-06-03 08:37:23 +0300

    Add type

--- error-utils/src/atmfjstc/lib/error_utils/__init__.py
+++ error-utils/src/atmfjstc/lib/error_utils/__init__.py
@@ -88,5 +88,5 @@ class WarningWithMessage(UserWarning):
     def __init__(self, message: str):
         super().__init__(message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.args[0]
