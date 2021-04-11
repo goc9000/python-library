78e0af20a29577d65cddc32b03ff0be3c478ff27
Atom of Justice <atmfjstc@protonmail.com>
2021-04-11 18:29:11 +0300

    Add support for asyncio OS X daemons

--- osx-daemon/src/atmfjstc/lib/osx_daemon/__init__.py
+++ osx-daemon/src/atmfjstc/lib/osx_daemon/__init__.py
@@ -1,8 +1,13 @@
 """
 A simple harness for daemons running on OS X (via `launchd`).
 
-It takes care of a few issues, such as setting up logging, setting up a PID file, handling fatals and routing
-termination signals.
+It takes care of a few issues, such as:
+
+- setting up logging
+- setting up a PID file (optional)
+- handling fatals
+- handling termination signals
+- setting up an async loop (for daemons using `asyncio`) and its interaction with signals
 
 By default logging is done to stderr, so the plist should contain a `StandardErrorPath` key redirecting it to some log
 file.
@@ -15,12 +20,13 @@ import os
 import errno
 import fcntl
 import time
+import asyncio
 
 from pathlib import Path
 from contextlib import contextmanager
 from argparse import ArgumentParser, Namespace
-from abc import ABCMeta, abstractmethod
-from typing import Text, NoReturn, Union, Optional, ContextManager
+from abc import abstractmethod, ABC
+from typing import Text, NoReturn, Union, Optional, ContextManager, Callable, Dict
 
 from pidlockfile import PIDLockFile, AlreadyLocked, LockTimeout
 
@@ -28,7 +34,7 @@ from atmfjstc.lib.cli_utils.errors import fail, DescriptiveError
 from atmfjstc.lib.cli_utils.root import is_root
 
 
-class OSXDaemon(metaclass=ABCMeta):
+class OSXDaemonBase(ABC):
     """
     A harness for daemons designed to be run via `launchd`.
     """
@@ -106,42 +112,17 @@ class OSXDaemon(metaclass=ABCMeta):
         """
         return logging.getLogger()
 
-    @abstractmethod
-    def run(self, raw_args: Namespace):
-        """
-        Override this with the main code of the daemon.
-
-        Note that `launchd` expects the code to implement a 'run in foreground' model. Do not apply typical UNIX daemon
-        tricks such as forking etc.
-
-        The code should run indefinitely (or until a termination is requested). Once this method returns, the daemon
-        will exit. To report errors or any other output, use the `logging` package. To report fatal errors, use `fail`
-        or throw some other Exception.
-
-        Args:
-            raw_args: The arguments, as parsed by the ArgumentParser
-        """
-        raise NotImplementedError
-
-    def on_terminate_requested(self):
-        """
-        Override this if desired, with the code that is to be executed when the daemon is asked to terminate.
-
-        By default this raises a SystemException, but more advanced daemons may want to instead put an element in a
-        queue, manage threads etc.
-        """
-        self.logger().info("Termination requested, shutting down...")
-        sys.exit(0)
-
     def go(self):
         """
         Starts the harness. Call this in `__main__` (and do nothing else).
         """
 
-        _catch_term = lambda _sig_num, _frame: self.on_terminate_requested()
+        _catch_term = lambda _sig_num, _frame: self._catch_signal()
 
-        signal.signal(signal.SIGTERM, _catch_term)
-        signal.signal(signal.SIGHUP, _catch_term)
+        old_sigs = {
+            signal.SIGTERM: signal.signal(signal.SIGTERM, _catch_term),
+            signal.SIGHUP: signal.signal(signal.SIGHUP, _catch_term),
+        }
 
         raw_args = self._parse_raw_args()
         self.setup_logging(raw_args if isinstance(raw_args, Namespace) else None)
@@ -154,7 +135,7 @@ class OSXDaemon(metaclass=ABCMeta):
                 fail("This daemon must be run as root")
 
             with self._maybe_grab_pidfile(raw_args):
-                self.run(raw_args)
+                self._run(raw_args, old_sigs)
         except KeyboardInterrupt:
             self.logger().info("Shut down by user")
             sys.exit(0)
@@ -164,6 +145,14 @@ class OSXDaemon(metaclass=ABCMeta):
         except Exception as e:
             self.logger().fatal("Unexpected exception", exc_info=e)
             sys.exit(-1)
+
+    @abstractmethod
+    def _run(self, raw_args: Namespace, old_sigs: Dict[signal.Signals, Callable]):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _catch_signal(self):
+        raise NotImplementedError
 
     def _parse_raw_args(self) -> Union[Namespace, Exception]:
         """
@@ -206,6 +195,108 @@ class OSXDaemon(metaclass=ABCMeta):
         finally:
             if lock is not None:
                 lock.__exit__()
+
+
+class OSXDaemon(OSXDaemonBase, ABC):
+    """
+    A harness for daemons designed to be run via `launchd`. This particular class is for daemons using a traditional
+    (non-`asyncio`) model.
+    """
+
+    @abstractmethod
+    def run(self, raw_args: Namespace):
+        """
+        Override this with the main code of the daemon.
+
+        Note that `launchd` expects the code to implement a 'run in foreground' model. Do not apply typical UNIX daemon
+        tricks such as forking etc.
+
+        The code should run indefinitely (or until a termination is requested). Once this method returns, the daemon
+        will exit. To report errors or any other output, use the `logging` package. To report fatal errors, use `fail`
+        or throw some other Exception.
+
+        Args:
+            raw_args: The arguments, as parsed by the ArgumentParser
+        """
+        raise NotImplementedError
+
+    def on_terminate_requested(self):
+        """
+        Override this if desired, with the code that is to be executed when the daemon is asked to terminate.
+
+        By default this raises a SystemException, but more advanced daemons may want to instead put an element in a
+        queue, manage threads etc.
+        """
+        self.logger().info("Termination requested, shutting down...")
+        sys.exit(0)
+
+    def _run(self, raw_args: Namespace, old_sigs: Dict[signal.Signals, Callable]):
+        self.run(raw_args)
+
+    def _catch_signal(self):
+        self.on_terminate_requested()
+
+
+class OSXAsyncDaemon(OSXDaemonBase, ABC):
+    """
+    A harness for daemons designed to be run via `launchd`. This particular class is for daemons using the `asyncio`
+    execution model.
+    """
+
+    _main_task: asyncio.Task
+
+    @abstractmethod
+    async def run(self, raw_args: Namespace):
+        """
+        Override this with the main code of the daemon (running as a coroutine).
+
+        Note that `launchd` expects the code to implement a 'run in foreground' model. Do not apply typical UNIX daemon
+        tricks such as forking etc.
+
+        The code should run indefinitely (or until a termination is requested). Once this method returns, the daemon
+        will exit. To report errors or any other output, use the `logging` package. To report fatal errors, use `fail`
+        or throw some other Exception.
+
+        Args:
+            raw_args: The arguments, as parsed by the ArgumentParser
+        """
+        raise NotImplementedError
+
+    async def on_terminate_requested(self):
+        """
+        Override this if desired, with the code that is to be executed when the daemon is asked to terminate.
+
+        By default this calls `cancel()` on the main task (the one that runs the `run()` method), but more advanced
+        daemons may enforce a more delicate or gradual shutdown.
+        """
+        self.logger().info("Termination requested, shutting down...")
+        self._main_task.cancel()
+
+    def _run(self, raw_args: Namespace, old_sigs: Dict[signal.Signals, Callable]):
+        # First restore old signals
+        for sig, handler in old_sigs.items():
+            signal.signal(sig, handler)
+
+        asyncio.run(self._async_run(raw_args))
+
+    async def _async_run(self, raw_args: Namespace):
+        loop = asyncio.get_running_loop()
+        loop.add_signal_handler(signal.SIGTERM, lambda: self._catch_async_signal())
+        loop.add_signal_handler(signal.SIGHUP, lambda: self._catch_async_signal())
+
+        try:
+            self._main_task = asyncio.create_task(self.run(raw_args))
+            await self._main_task
+        except asyncio.CancelledError:
+            pass
+
+    def _catch_signal(self):
+        # Note: this only applies to signals caught during startup (before the async loop is set up)
+        self.logger().info("Termination requested, shutting down...")
+        sys.exit(0)
+
+    def _catch_async_signal(self):
+        asyncio.create_task(self.on_terminate_requested())
 
 
 class _ArgumentParser(ArgumentParser):
