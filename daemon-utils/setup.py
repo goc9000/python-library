27d524d83ec488e2a8ff93985a88bbc191ce1d37
Atom of Justice <atmfjstc@protonmail.com>
2022-03-20 16:14:01 +0200

    Initial commit for daemon-utils package

---
+++ daemon-utils/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atmfjstc-daemon-utils',
+    version='0.0.0',
+
+    author_email='atmfjstc@protonmail.com',
+
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    
+    install_requires=[
+    ],
+
+    zip_safe=True,
+
+    description="Common mechanisms used by daemons for responding to requests, handling subtasks etc.",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Typing :: Typed",
+    ],
+    python_requires='>=3.9',
+)
---
+++ daemon-utils/src/atmfjstc/lib/daemon_utils/__init__.py
@@ -0,0 +1,4 @@
+"""
+Common mechanisms used by daemons for responding to requests, handling subtasks etc.
+"""
+
