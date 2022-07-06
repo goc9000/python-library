36773d7345e739c71ec19c4cb0bf263c625b7387
Atom of Justice <atmfjstc@protonmail.com>
2022-07-06 15:07:11 +0300

    Bump version to ensure autobuild picks up the corrections to the package spec

--- abstract-codegen/setup.py
+++ abstract-codegen/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='atmfjstc-abstract-codegen',
-    version='1.1.0',
+    version='1.1.1',
 
     author_email='atmfjstc@protonmail.com',
 
