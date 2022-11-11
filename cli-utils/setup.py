35d480e7a9d1811a744e67ba671e9b4684b1f4f4
Atom of Justice <atmfjstc@protonmail.com>
2022-11-11 16:11:33 +0200

    Switch to static package metadata, single-sourced version and native namespace packages

--- cli-utils/setup.py
+++ cli-utils/setup.py
@@ -1,31 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='atmfjstc-cli-utils',
-    version='1.8.2',
 
-    author_email='atmfjstc@protonmail.com',
-
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-
-    install_requires=[
-        'termcolor>=1, <2',
-        'colorama>=0.4.0, <2',
-        'atmfjstc-text-utils>=1.3, <2',
-        'atmfjstc-error-utils>=1, <2',
-    ],
-
-    zip_safe=True,
-
-    description="Utilities for command-line programs",
-
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Environment :: Console",
-        "Typing :: Typed",
-    ],
-    python_requires='>=3.7',
-)
+setup()
--- cli-utils/src/atmfjstc/lib/cli_utils/__init__.py
+++ cli-utils/src/atmfjstc/lib/cli_utils/__init__.py
@@ -1,3 +1,6 @@
 """
 A collection of utilities for command-line programs, particularly interactive scripts.
 """
+
+
+__version__ = '1.8.2'
