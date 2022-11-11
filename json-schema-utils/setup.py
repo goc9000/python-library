394882e11f919085cacfd0a9a869a1ab49552bd5
Atom of Justice <atmfjstc@protonmail.com>
2022-11-11 17:59:50 +0200

    Switch to static package metadata, single-sourced version and native namespace packages

--- json-schema-utils/setup.py
+++ json-schema-utils/setup.py
@@ -1,28 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='atmfjstc-json-schema-utils',
-    version='0.2.2',
 
-    author_email='atmfjstc@protonmail.com',
-
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-
-    install_requires=[
-        'atmfjstc-py-lang-utils>=1.11.3, <2',
-        'jsonschema>=3, <4',
-    ],
-
-    zip_safe=True,
-
-    description="Utilities for working with JSONSchemas",
-
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-        "Typing :: Typed",
-    ],
-    python_requires='>=3.7',
-)
+setup()
--- json-schema-utils/src/atmfjstc/lib/json_schema_utils/__init__.py
+++ json-schema-utils/src/atmfjstc/lib/json_schema_utils/__init__.py
@@ -1,3 +1,6 @@
+__version__ = '0.2.2'
+
+
 JSONSchema = dict
 """
 Simple type for marking a JSON Schema. May be developed later, for now just a dict.
