7b44fc017c478b73b8aab1fbaaafcb5444c12443
Atom of Justice <atmfjstc@protonmail.com>
2024-06-08 15:26:19 +0300

    Switch to static package metadata, single-sourced version and native namespace packages

--- archive-forensics/setup.py
+++ archive-forensics/setup.py
@@ -1,31 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='atmfjstc-archive-forensics',
-    version='0.4.2',
 
-    author_email='atmfjstc@protonmail.com',
-
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-
-    install_requires=[
-        'atmfjstc-iso-timestamp>=1.1.0, <2',
-        'atmfjstc-binary-utils>=1.2.0, <2',
-        'atmfjstc-os-forensics>=0.2.1, <2',
-        'atmfjstc-py-lang-utils>=1.11.0, <2',
-    ],
-
-    zip_safe=True,
-
-    description="Utilities for decoding archive format-specific data enums and binary structures",
-
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: System :: Archiving",
-        "Typing :: Typed",
-    ],
-    python_requires='>=3.7',
-)
+setup()
--- archive-forensics/src/atmfjstc/lib/archive_forensics/__init__.py
+++ archive-forensics/src/atmfjstc/lib/archive_forensics/__init__.py
@@ -5,3 +5,6 @@ This is not intended to be a collection of complete archive parsers like `zipfil
 on individual enums, structures and tidbits that are not fully covered or decoded by the former. The utilities herein
 can also form a base for building alternative ZIP, RAR etc. parsers of our own.
 """
+
+
+__version__ = '0.4.3'
