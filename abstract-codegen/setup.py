97ec1937aad0d685545206c4811d08407cfc2d41
Atom of Justice <atmfjstc@protonmail.com>
2024-06-08 15:55:36 +0300

    Switch to static package metadata, single-sourced version and native namespace packages

--- abstract-codegen/setup.py
+++ abstract-codegen/setup.py
@@ -1,30 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='atmfjstc-abstract-codegen',
-    version='1.1.1',
 
-    author_email='atmfjstc@protonmail.com',
-
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-
-    install_requires=[
-        'atmfjstc-py-lang-utils>=1.3, <2',
-        'atmfjstc-ast>=1.4, <2',
-        'atmfjstc-text-utils>=1.4, <2',
-    ],
-
-    zip_safe=True,
-
-    description="A model and utilities for greatly easing code generation for nearly any language",
-
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Code Generators"
-        "Typing :: Typed",
-    ],
-    python_requires='>=3.7',
-)
+setup()
