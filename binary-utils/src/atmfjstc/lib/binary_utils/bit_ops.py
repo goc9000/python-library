7fb54b1b9c3c0cc04bdf48c1ed5d4faa229b756b
Atom of Justice <atmfjstc@protonmail.com>
2025-01-05 02:27:10 +0200

    Clarify order of results

--- binary-utils/src/atmfjstc/lib/binary_utils/bit_ops.py
+++ binary-utils/src/atmfjstc/lib/binary_utils/bit_ops.py
@@ -16,6 +16,8 @@ def get_set_bits(value: int) -> List[int]:
     """
     Returns a list of all set bits in an integer. E.g. for 24, returns [3,4] since 24 = (1 << 3) + (1 << 4)
 
+    The bit indexes are always returned in ascending order.
+
     Negative values are not supported as this is mainly intended for taking apart values representing masks and flags.
     """
     if value < 0:
@@ -28,6 +30,8 @@ def split_powers2(value: int) -> List[int]:
     """
     Returns a list of the powers of 2 that make up an integer. E.g. for 24, returns [8, 16], since
     8 + 16 = 2^3 + 2^4 == 24
+
+    The powers are always returned in ascending order.
 
     Negative values are not supported as this is mainly intended for taking apart values representing masks and flags.
     """
